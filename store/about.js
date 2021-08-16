681d27bee95f63361657d63a0ed801ea8caf4822
gu1233 <gde102@naver.com>
2021-08-16 16:40:22 +0900

    Add nuxt project

---
+++ store/about.js
@@ -0,0 +1,9 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'GuDaeun',
+    email: 'gde102@naver.com',
+    phone: '+82-10-4930-9510',
+    image: 'https://avatars.githubusercontent.com/u/59717481?v=4'
+  })
+}
